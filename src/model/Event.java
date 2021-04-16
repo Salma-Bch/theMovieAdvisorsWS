90ba07c8fb8450e13787c23308b7bf5a7a309413
S-BCH <S-BCH@users.noreply.github.com>
2021-04-16 17:28:35 +0200

    - Modifications. - Ajout des commentaires.

--- src/model/Event.java
+++ src/model/Event.java
@@ -1,5 +1,13 @@
 package model;
 
+/**
+ * 
+ * Event est la classe de donnée d'un évènement.
+ * 
+ * @author Salma BENCHELKHA & Mouncif LEKMITI
+ * @version 1.0
+ *
+ */
 public class Event {
 	private int id;
 	private String name;
@@ -7,6 +15,16 @@ public class Event {
 	private String area;
 	private Artist artist;
 	
+	/**
+	 * Constructeur de la classe Event.
+	 * 
+	 * @param id
+	 * @param name
+	 * @param date
+	 * @param area	 
+	 * @param artist	 	 
+	 *
+	 */
 	public Event(int id, String name, String date, String area, Artist artist) {
 		super();
 		this.id = id;
@@ -16,37 +34,112 @@ public class Event {
 		this.artist = artist;
 	}
 	
+	/**
+	 * Méthode permettant de récupérer l'id de l'évènement.
+	 *  
+	 * @return L'id de l'évènement (type int).
+	 *
+	 */
 	public int getId() {
 		return id;
 	}
+	
+	/**
+	 * Méthode permettant de modifier l'id de l'évènement.
+	 *  
+	 * @param id
+	 *
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	/**
+	 * Méthode permettant de récupérer le nom de l'évènement.
+	 *  
+	 * @return L'id de l'évènement (type int).
+	 *
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * Méthode permettant de modifier le nom de l'évènement.
+	 *  
+	 * @param name
+	 *
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * Méthode permettant de récupérer la date de l'évènement.
+	 *  
+	 * @return La date de l'évènement (type String).
+	 *
+	 */
 	public String getDate() {
 		return date;
 	}
+	
+	/**
+	 * Méthode permettant de modifier la date de l'évènement.
+	 *  
+	 * @param date
+	 *
+	 */
 	public void setDate(String date) {
 		this.date = date;
 	}
+	
+	/**
+	 * Méthode permettant de récupérer la région de l'évènement.
+	 *  
+	 * @return La région de l'évènement (type String).
+	 *
+	 */
 	public String getArea() {
 		return area;
 	}
+	
+	/**
+	 * Méthode permettant de modifier la région de l'évènement.
+	 *  
+	 * @param area
+	 *
+	 */
 	public void setArea(String area) {
 		this.area = area;
 	}
+	
+	/**
+	 * Méthode permettant de récupérer l'artiste de l'évènement.
+	 *  
+	 * @return L'artiste de l'évènement (type Artiste).
+	 *
+	 */
 	public Artist getArtist() {
 		return artist;
 	}
+	
+	/**
+	 * Méthode permettant de modifier l'artiste de l'évènement.
+	 *  
+	 * @param artist
+	 *
+	 */
 	public void setArtist(Artist artist) {
 		this.artist = artist;
 	}
 
+	/**
+	 * Méthode permettant de récupérer toutes les informations de l'évènement.
+	 *  
+	 * @return  Toutes les informations de l'évènement (type String).
+	 *
+	 */
 	@Override
 	public String toString() {
 		return "Event [id=" + id + ", name=" + name + ", date=" + date + ", area=" + area + ", artist=" + artist + "]";
--- src/service/EventManagement.java
+++ src/service/EventManagement.java
@@ -4,26 +4,73 @@ import javax.jws.*;
 import model.Artist;
 import model.Event;
 
+/**
+ * EventManagement est l'interface du service d'un évènement.
+ * 
+ * @author Salma BENCHELKHA & Mouncif LEKMITI
+ * @version 1.0
+ *
+ */
 @WebService(name = "EventManagement",targetNamespace = "http://service/")
 public interface EventManagement {
-		
+	
+	/**
+	 * Méthode permettant d'ajouter un évènement.
+	 *  
+	 * @param event
+	 * @return L'id de l'évenement ajouté ou -1 en cas d'erreur.
+	 *
+	 */
 	@WebMethod(operationName = "addEvent", action = "urn:AddEvent")
 	@WebResult(name = "id")
 	public int addEvent(@WebParam(name = "event") Event event);
 	
+	/**
+	 * Méthode permettant de supprimer un évènement.
+	 *  
+	 * @param id
+	 * @return Retourne "true" si l'évènement a été rajouter ou "false" en cas d'erreur.
+	 *
+	 */
 	/*
 	@WebMethod(operationName = "deleteEvent", action = "urn:DeleteEvent")
 	@WebResult(name = "deleted")
 	public boolean deleteEvent(@WebParam(name = "id") int id);
+	*/
 	
+	/**
+	 * Méthode permettant de récupérer les évènements à partir d'une région.
+	 *  
+	 * @param area
+	 * @return Retourne un tableau d'évènement.
+	 *
+	 */
+	/*
 	@WebMethod(operationName = "getEventsByArea", action = "urn:GetEventsByArea")
 	@WebResult(name = "event")
 	public Event[] getEventsByArea(@WebParam(name = "area") String area);
+	*/
 	
+	/**
+	 * Méthode permettant de récupérer les évènements à partir d'un artiste.
+	 *  
+	 * @param artiste
+	 * @return Retourne un tableau d'évènement.
+	 *
+	 */
+	/*
 	@WebMethod(operationName = "getEventsByArtist", action = "urn:GetEventsByArtist")
 	@WebResult(name = "event")
 	public Event[] getEventsByArtist(@WebParam(name = "artist") Artist artist);
+	*/
 	
+	/**
+	 * Méthode permettant de récupérer tout les évènements.
+	 *  
+	 * @return Retourne un tableau d'évènement.
+	 *
+	 */
+	/*
 	@WebMethod(operationName = "getAllEvents", action = "urn:GetAllEvents")
 	@WebResult(name = "events")
 	public Event[] getAllEvents();
